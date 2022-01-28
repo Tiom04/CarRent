4d5f9eeb4548950dba425225b7aa44530ed9fb82
Tiom04 <tiombanar@gmail.com>
2022-01-28 19:45:05 +0200

    calculate hash for a string (class Utils)

--- CarRent/CarRent/Form1.cs
+++ CarRent/CarRent/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -37,6 +39,28 @@ namespace CarRent
             }
             //MessageBox.Show($"Login = {log}, Password = {pass}");
 
+            try
+            {
+                SHA256 sha = SHA256.Create();
+                string hashPassword = Utils.HashPassword(pass);
+                //get active user by username, password(hashpassword)
+                var user = 1;//user from db
+                if(user == null)
+                {
+                    MessageBox.Show("Incorrect password or login");
+                }
+                else//user exists
+                {
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         private void Login_Load(object sender, EventArgs e)
